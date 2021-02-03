33092dcc6408ccb5307a5849489a6853f412faf7
SOVITH3 <carlos.arratia.00@gmail.com>
2021-02-03 00:45:00 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,107 @@
+//SERVER
+const cors = require('cors');
+const app = require('express')();
+app.use(cors());
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//PORT COM TEMPERATURA ARDUINO NANO
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM17', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+
+
+//JONNY FIVE
+const five = require('johnny-five'); 
+
+//Placa Arduino UNO
+var board = new five.Board({ port: "COM10" });
+board.on("ready", function() {
+  let regado = false;
+  let regadoAut = true;
+  let hidro = new five.Led(13);
+  let luz = new five.Led(12);
+  let tempRiego = 47;
+  hidro.off();
+
+  //SOCKET COMUNICACION servidor/cliente
+  io.on('connection', socket => {
+    console.log("Usuario Conectado");
+
+    //Activar/Desactivar "Riego Automatico"
+    socket.on('regadoAutomatico', function(data) {
+      if(data.estado === 'on'){
+        regadoAut = true;
+        io.sockets.emit('regado',{estado:'Riego Manual Desactivao'});
+        hidro.on()
+        regado = false;
+      }else if(data.estado === 'off'){
+        regadoAut = false; 
+        hidro.off();
+        io.sockets.emit('regado',{estado:'Riego Manual Activado'});
+      }
+   });
+
+    //Listeners hacia el Cliente
+      socket.on('hidro', function(data) {
+        //REGAR 2 SEG y esta desactivado el Riego Manual
+        if(data.estado === 'on' && !regadoAut){
+          hidro.on();
+          setTimeout(() => { hidro.off()}, 2000);
+          io.sockets.emit('regado',{estado:'Se ha regado'});
+        }else if(data.estado === 'on' && regadoAut){
+          io.sockets.emit('regado',{estado:'!El regado manual esta desactivado'});
+        }
+     });
+   //ENCENDER/APAGAR LUZ
+   socket.on('luz', function(data) {
+      if(data.estado === 'on'){
+        luz.on();
+      }else{
+        luz.off();
+      }
+    });
+  });
+
+  //PUERTO DEL SERVIDOR
+  server.listen(3000);
+  app.get('/', (req, res) => {
+    //PAGINA DE PRUEBA
+    res.sendFile(__dirname + '/index.html');
+  });
+
+//PUERTO SENSOR
+  port.on("open", () => {
+    //LOG
+    console.log('Conección establecida');
+  });
+  parser.on('data', data => {
+    console.log(String(data))
+    let th = {
+      humedad: (String(data).substring(0,6)),
+      temperatura: (String(data).substring(6,12)),
+      humedadInt : parseInt(String(data).substring(0,2))
+    }
+    //Emitir al Cliente con la data de temperatura/humedad
+    io.sockets.emit('data',th);
+    console.log(th)
+
+    //Riego Automatico  si esta activado. Por 2 segundos si la humedad es menor a 45% y no a sido regado en 20 min
+   if(regadoAut){
+      if(!regado){
+        if(th.humedadInt<tempRiego){
+          hidro.on();
+          setTimeout(() => { hidro.off()}, 2000);
+          regado = true;
+          //Establecer tiempo de regado automatico (Evitar Bucle Infinito mientras no suba de golpe la humedad)
+          setTimeout(() => {regado=false}, 10000);
+          //EMITIR ESTADO RIEGO
+          io.sockets.emit('regado',{estado:'Regado automático, Actualizacion cada 20 minutos'});
+        }
+      }else{
+          io.sockets.emit('regado',{estado:'Aún no se cumplen los 20 minutos'});
+      }
+  }
+  });
+});
